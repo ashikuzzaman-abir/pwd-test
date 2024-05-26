54d1fd894d65404b3742cc90be98ce2d3aa53252
ashikuzzaman-abir <ashikuzzamanabir@outlook.com>
2024-05-26 15:55:48 +0600

    asasa

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
-import withPWAinit from 'next-pwa';
+// import withPWAinit from 'next-pwa';
 
-const withPWA = withPWAinit({
-	pwa: {
-		dest: 'public',
-	},
-});
+// const withPWA = withPWAinit({
+// 	pwa: {
+// 		dest: 'public',
+// 		swSrc: 'service-worker.js',
+// 	},
+// });
 
-const nextConfig = withPWA({});
+// const nextConfig = withPWA({});
+
+const nextConfig = {};
 
 export default nextConfig;

47fc7b60993d70bdf66ff1576057198e2471db99
arefin.aareef <arefin.aareef@gmail.com>
2024-05-27 14:52:09 +0600

    state added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Form from '@/components/Form';
 import styles from './page.module.css';
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import {
 	Container,
 	Flex,
@@ -39,9 +39,32 @@ const Form: FC<FormProps> = ({}) => {
 // VARIABLES
 
 // STYLES
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+const [message, setMessage] = useState('');
+
+// console.log('data', name, email, message);
+
 
 // FUNCTIONS
+const handleSubmit = (e: React.FormEvent) => {
+	e.preventDefault();
 
+	// Get the existing data
+	let existingData = localStorage.getItem('formData');
+
+	// If no existing data, create an array
+	// Otherwise, convert the localStorage string to an array
+	existingData = existingData ? JSON.parse(existingData) : [];
+
+	// Add new data to localStorage Array
+	(existingData as unknown as Array<any>).push({ name, email, message });
+
+	// Save back to localStorage
+	localStorage.setItem('formData', JSON.stringify(existingData));
+
+	console.log(existingData);
+};
 // EFFECTS
 
 // COMPONENTS
@@ -59,93 +82,22 @@ const Form: FC<FormProps> = ({}) => {
 					<Box p={4}>
 						<Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
 							<WrapItem>
-								<Box>
-									<Heading>Contact</Heading>
-									<Text mt={{ sm: 3, md: 3, lg: 5 }} color='gray.500'>
-										Fill up the form below to contact
-									</Text>
-									<Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
-										<VStack pl={0} spacing={3} alignItems='flex-start'>
-											<Button
-												size='md'
-												height='48px'
-												width='200px'
-												variant='ghost'
-												color='#DCE2FF'
-												_hover={{ border: '2px solid #1C6FEB' }}
-												leftIcon={<MdPhone color='#1970F1' size='20px' />}
-											>
-												+91-988888888
-											</Button>
-											<Button
-												size='md'
-												height='48px'
-												width='200px'
-												variant='ghost'
-												color='#DCE2FF'
-												_hover={{ border: '2px solid #1C6FEB' }}
-												leftIcon={<MdEmail color='#1970F1' size='20px' />}
-											>
-												hello@abc.com
-											</Button>
-											<Button
-												size='md'
-												height='48px'
-												width='200px'
-												variant='ghost'
-												color='#DCE2FF'
-												_hover={{ border: '2px solid #1C6FEB' }}
-												leftIcon={<MdLocationOn color='#1970F1' size='20px' />}
-											>
-												Karnavati, India
-											</Button>
-										</VStack>
-									</Box>
-									<HStack
-										mt={{ lg: 10, md: 10 }}
-										spacing={5}
-										px={5}
-										alignItems='flex-start'
-									>
-										<IconButton
-											aria-label='facebook'
-											variant='ghost'
-											size='lg'
-											isRound={true}
-											_hover={{ bg: '#0D74FF' }}
-											icon={<MdFacebook size='28px' />}
-										/>
-										<IconButton
-											aria-label='github'
-											variant='ghost'
-											size='lg'
-											isRound={true}
-											_hover={{ bg: '#0D74FF' }}
-											icon={<BsGithub size='28px' />}
-										/>
-										<IconButton
-											aria-label='discord'
-											variant='ghost'
-											size='lg'
-											isRound={true}
-											_hover={{ bg: '#0D74FF' }}
-											icon={<BsDiscord size='28px' />}
-										/>
-									</HStack>
-								</Box>
-							</WrapItem>
-							<WrapItem>
 								<Box bg='white' borderRadius='lg'>
 									<Box m={8} color='#0B0E3F'>
 										<VStack spacing={5}>
-											<FormControl id='name'>
+											<FormControl id='name' onSubmit={handleSubmit}>
 												<FormLabel>Your Name</FormLabel>
 												<InputGroup borderColor='#E0E1E7'>
 													<InputLeftElement
 														pointerEvents='none'
 														children={<BsPerson color='gray.800' />}
 													/>
-													<Input type='text' size='md' />
+													<Input
+														type='text'
+														size='md'
+														value={name}
+														onChange={e => setName(e.target.value)}
+													/>
 												</InputGroup>
 											</FormControl>
 											<FormControl id='name'>
@@ -155,7 +107,12 @@ const Form: FC<FormProps> = ({}) => {
 														pointerEvents='none'
 														children={<MdOutlineEmail color='gray.800' />}
 													/>
-													<Input type='text' size='md' />
+													<Input
+														type='text'
+														size='md'
+														value={email}
+														onChange={e => setEmail(e.target.value)}
+													/>
 												</InputGroup>
 											</FormControl>
 											<FormControl id='name'>
@@ -166,6 +123,8 @@ const Form: FC<FormProps> = ({}) => {
 														borderRadius: 'gray.300',
 													}}
 													placeholder='message'
+													value={message}
+													onChange={e => setMessage(e.target.value)}
 												/>
 											</FormControl>
 											<FormControl id='name' float='right'>
@@ -174,6 +133,7 @@ const Form: FC<FormProps> = ({}) => {
 													bg='#0D74FF'
 													color='white'
 													_hover={{}}
+													type='submit'
 												>
 													Send Message
 												</Button>

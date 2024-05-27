f628aa365a8332b8f811ac9850ccf402dbffaaca
arefin.aareef <arefin.aareef@gmail.com>
2024-05-27 16:00:47 +0600

    page added

---
+++ src/app/response/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+import Card from '@/components/Card';
+import { Grid } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import React from 'react';
+
+type pageProps = {};
+
+const page: NextPage<pageProps> = ({}) => {
+	// HOOKS
+
+	// STATE
+
+	// VARIABLES
+
+	// STYLES
+
+	// FUNCTIONS
+
+	// EFFECTS
+
+	// COMPONENTS
+
+	return (
+		<Grid templateColumns='1fr 1fr 1fr'>
+			<Card />
+			<Card />
+		</Grid>
+	);
+};
+
+export default page;
